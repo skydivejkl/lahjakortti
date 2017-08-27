9c527a6d6adb90e4ef1962049d83b655e1c714ec
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-08-27 14:25:57 +0300

    Handle overrides

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import * as Koa from "koa";
 import * as Router from "koa-router";
 import * as serve from "koa-static";
 import * as mount from "koa-mount";
+import * as body from "koa-bodyparser";
+import * as fsp from "mz/fs";
 
 import * as fs from "fs";
 
@@ -14,20 +16,28 @@ const PUBLIC = __dirname + "/../public";
 var app = new Koa();
 var router = new Router();
 
-function makeKey() {
-    var text = "";
-    var possible = "abcdefghijklmnopqrstuvwxyz";
-
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-}
-
 const serveIndex = async (ctx: Router.IRouterContext) => {
     const s = fs.createReadStream(PUBLIC + "/index.html");
     ctx.type = "text/html";
     ctx.body = s;
+};
+
+const fileExists = async (path: string) => {
+    let stat = null;
+
+    try {
+        stat = await fsp.stat(path);
+    } catch (err) {
+        if (err.code == "ENOENT") {
+            return false;
+        }
+    }
+
+    if (stat) {
+        return stat.isFile;
+    }
+
+    return false;
 };
 
 router.get("/tandem", serveIndex);
@@ -35,6 +45,7 @@ router.get("/email", serveIndex);
 
 router.post("/tandem", async ctx => {
     const options: {id?: string; name?: string} = ctx.query;
+    const force = Boolean(ctx.request.body.force);
 
     if (!options.id) {
         throw new Error("id missing");
@@ -48,26 +59,35 @@ router.post("/tandem", async ctx => {
         throw new Error("name missing");
     }
 
-    const secret = makeKey();
     const url = INTERNAL_ADDRESS + ctx.req.url + "&pdf=1";
-    const pdfPath = `/pdf/${options.id}-${secret}.pdf`;
+    const pdfPath = `/pdf/${options.id}.pdf`;
+    const pdfFSPath = PUBLIC + pdfPath;
+
+    if (!force && (await fileExists(pdfFSPath))) {
+        ctx.body = {
+            exists: true,
+        };
+        return;
+    }
+
     console.log("Opening " + url);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url, {waitUntil: "networkidle"});
     await page.pdf({
-        path: PUBLIC + pdfPath,
+        path: pdfFSPath,
         format: "A4",
         printBackground: true,
     });
     browser.close();
+
     ctx.body = {
-        secret,
-        id: options.id,
+        ok: true,
     };
 });
 
+app.use(body());
 app.use(router.routes()).use(router.allowedMethods());
 app.use(mount("/assets", serve(PUBLIC)));
 
