5e686b1f76bec314b77b290e9fd910be6ac52af2
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-08-29 00:08:28 +0300

    Add make.sh

---
+++ make.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -eu
+
+export PATH=$(pwd)/node_modules/.bin/:$PATH
+self=$0
+
+set -x
+
+case ${1:-} in
+   deps)
+        npm install
+   ;;
+   build-client)
+        webpack -p
+   ;;
+   build-server)
+        cd src/server
+        tsc
+        cd -
+   ;;
+   serve-client)
+        webpack-dev-server  --port  8081 --inline
+   ;;
+   server)
+        cd src/server
+        exec ts-node server.ts
+   ;;
+   server-dev)
+        DISABLE_AUTH=1
+        exec nodemon --ext ts --exec sh $self server
+   ;;
+   "")
+        $self deps
+        $self build-client
+        $self build-server
+   ;;
+   *)
+        echo "Bad task"
+        exit 1
+   ;;
+esac
